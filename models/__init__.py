22d5ede93f973908d5cad8c790ff99d83c2b3b56
Youness Elalouani <younesselalouani1995@gmail.com>
2024-01-28 15:50:52 +0100

    show the list of states

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from os import getenv
-
+import sys
+sys.path.append(
+    '/Users/mac/Documents/airBnB_clone_v2-merged/AirBnB_clone_v2/.venv/lib/python3.12/site-packages')
 
 HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+  module that run the app server
+"""
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def state_list():
+    data = storage.all()
+
+    states = []
+    for x, y in data.items():
+        if x[:5] == "State":
+            states.append(y)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def remove_session(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
