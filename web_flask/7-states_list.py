6b3c86edb6c6b4155a2fcd94d3105cf30e94eb0a
Youness Elalouani <younesselalouani1995@gmail.com>
2024-01-28 19:02:19 +0100

    fix the list of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,27 +10,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route("/states_list", strict_slashes=False)
+def state_list():
+    """
+      function get the data from DB and handle the route /states_list
+    """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
 @app.teardown_appcontext
 def remove_session(self):
     """
       function run after every request
     """
     storage.close()
-
-
-@app.route("/states_list", strict_slashes=False)
-def state_list():
-    """
-      function get the data from DB and handle the route /states_list
-    """
-    states = []
-    data = storage.all(State).values()
-    for item in data:
-        states.append({
-            "id": item.id,
-            "name": item.name
-        })
-    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
